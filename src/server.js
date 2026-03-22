eb0a3052813513507e2537757fb6d0dec42c7fd5
razors <r4zorsjrvs@gmail.com>
2026-03-22 04:15:41 -0300

    fix: trust proxy Railway rate-limit

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 const rateLimit = require('express-rate-limit');
